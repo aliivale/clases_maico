01a6030527e2f40ceac0cef49c09f4e308203825
aliivale <alivaleria.2003.anampa@gmail.com>
2024-04-22 17:02:09 -0300

    ejercicio de venta y ejercicio de signo solo aries

--- ejercicio1/src/ejercicio1/Ejercicio1.java
+++ ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -24,10 +24,62 @@ public class Ejercicio1 {
             mostrar.print("Ingrese el valor del angulo 2: ");
             int lado2 = Integer.valueOf(leer.readLine());
             mostrar.print("La respuesta es: " + log.calcularangulo(lado1, lado2)+ "\n");
-            */
+            
             mostrar.print("Ingrese la temperatura en grados Fahrenheit: ");
             int grado = Integer.valueOf(leer.readLine());
             mostrar.print("La temperatura en celcius es: "+ log.calculargrado(grado)+ "\n");
+            
+            
+            mostrar.print(" BIENVENIDO A TECNOMUNDO!!! \n ");
+            mostrar.print("------------------------------------ \n ");
+            mostrar.print("Ingrese el producto que quiere: ");
+            String producto1 = leer.readLine();
+            mostrar.print(" Precio: $" );
+            double precio1 = Float.valueOf(leer.readLine());
+            mostrar.print(" \n PLANES DISPONIBLES: \n");
+            mostrar.print(" Plan 1: Abonando el 100% en el primer pago, te regalamos un descuento del 10%. \n Plan 2: Abonando el 50% en el primer pago, tenes acceso a 2 cuotas para pagar el resto incrementando al precio base un 10%. \n Plan 3: Abonando el 25% en el primer pago, tenes acceso a 5 cuotas para pagar el resto incrementando al precio base un 15%. \n Plan 4: Abonando el 60% en 4 cuotas, tenes acceso a 4 cuotas para pagar el resto incrementando al precio base un 25%. \n ");
+            mostrar.print("\n Ingrese el numero del plan seleccionado: ");
+            int plan1 = Integer.valueOf(leer.readLine());
+            //calculamosprecio.()
+            mostrar.print("\n El precio final es: " + log.calcularprecio(precio1, plan1) + "\n");
+            */
+            
+            /*
+            
+            
+            SAGITARIO
+            22 NOVIEMBRE Y 21 DE DICIEMBRE
+            LEO 
+            22 DE JULIO A 23 DE AGOSTO
+            
+            
+            
+            
+            22 7 AL 23 8
+            DIA : 8
+            MES : 8
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            */
+            
+            mostrar.print("Dia: \n");
+            int dia = Integer.valueOf(leer.readLine());
+            mostrar.print("Mes: \n");
+            int mes = Integer.valueOf(leer.readLine());
+            mostrar.print("El signo es: " + log.calcularsigno(dia, mes));
+        
+            
             
             
             
--- ejercicio1/src/ejercicio1/logica.java
+++ ejercicio1/src/ejercicio1/logica.java
@@ -9,13 +9,15 @@ package ejercicio1;
  * @author aliva
  */
 public class logica {
-    logica(){}
-    
-    float multiplicar(float v, int h){
+
+    logica() {
+    }
+
+    float multiplicar(float v, int h) {
         float sueldo = v * h;
         return sueldo;
     }
-    
+
     int calcularangulo(int l1, int l2) {
 
         int suma = l1 + l2;
@@ -23,13 +25,53 @@ public class logica {
         int rta = 180 - suma;
 
         return rta;
-        
-     
+
     }
-    
-    float calculargrado(int gf){
+
+    float calculargrado(int gf) {
         System.out.println(gf);
-        float c = ((gf -32)* 5/9);
+        float c = ((gf - 32) * 5 / 9);
         return c;
     }
+
+    double calcularprecio(double precio, int plan) {
+
+        double preciofinal = 0;
+
+        if (plan == 1) {
+
+            preciofinal = precio - (precio * 0.1);
+
+        } else if (plan == 2) {
+
+            preciofinal = precio + (precio * 0.1);
+        } else if (plan == 3) {
+
+            preciofinal = precio + (precio * 0.15);
+        } else {
+
+            preciofinal = precio + (precio * 0.25);
+        }
+
+        return preciofinal;
+
+    }
+
+    String calcularsigno(int dia, int mes) {
+        
+        String signo = "";
+        if((dia >21 && mes == 3) || (dia <19 && mes == 4)){
+            
+            signo = "Aries";
+            
+            
+        
+        }
+        /*
+        else if(){
+        }
+        */
+        return signo;
+    }
+
 }
